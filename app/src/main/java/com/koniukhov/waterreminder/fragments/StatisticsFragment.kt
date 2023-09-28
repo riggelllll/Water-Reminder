4a7a5af3cd7f57feaba9014dcde9a50c1b6134ab
Vladyslav Koniukhov <riggelllll@gmail.com>
2023-09-28 11:18:26 +0300

    WR-9 Create empty fragments and their layouts

---
+++ app/src/main/java/com/koniukhov/waterreminder/fragments/StatisticsFragment.kt
@@ -0,0 +1,32 @@
+package com.koniukhov.waterreminder.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.koniukhov.waterreminder.databinding.StatisticsFragmentBinding
+
+class StatisticsFragment : Fragment() {
+
+    private var _binding: StatisticsFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = StatisticsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
