ff05e64ac612a47e415b0b0aeedd4e9fd58295ab
Vladyslav Koniukhov <riggelllll@gmail.com>
2023-09-29 13:09:27 +0300

    WR-11 Create bottom navigation

--- app/src/main/java/com/koniukhov/waterreminder/MainActivity.kt
+++ app/src/main/java/com/koniukhov/waterreminder/MainActivity.kt
@@ -3,11 +3,26 @@ package com.koniukhov.waterreminder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.koniukhov.waterreminder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bindBottomBarToNavController()
+    }
+
+    private fun bindBottomBarToNavController(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
