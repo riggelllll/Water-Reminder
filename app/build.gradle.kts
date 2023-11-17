e66df3e1a2caf21b7d7ab6afc1809acf335faff8
Vladyslav Koniukhov <riggelllll@gmail.com>
2023-11-17 15:02:30 +0200

    WR-33 Create DailyWater History

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ dependencies {
 
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
 
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.3")
