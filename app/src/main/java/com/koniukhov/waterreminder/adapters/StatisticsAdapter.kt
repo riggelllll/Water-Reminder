47bcfa835861f41fc79dca54b8689e438495076e
Vladyslav Koniukhov <riggelllll@gmail.com>
2023-11-18 10:55:09 +0200

    WR-35 Fix a bug wrong tab selected in statistics

--- app/src/main/java/com/koniukhov/waterreminder/adapters/StatisticsAdapter.kt
+++ app/src/main/java/com/koniukhov/waterreminder/adapters/StatisticsAdapter.kt
@@ -1,14 +1,13 @@
 package com.koniukhov.waterreminder.adapters
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.koniukhov.waterreminder.fragments.DailyStatisticsFragment
 import com.koniukhov.waterreminder.fragments.MonthlyStatisticsFragment
 
 const val ITEMS_SIZE = 2
 const val DAILY_FRAGMENT = 0
-class StatisticsAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+class StatisticsAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int {
         return ITEMS_SIZE
     }
