167704274971e8cfabb5f5d981433eccf57729ef
Vladyslav Koniukhov <riggelllll@gmail.com>
2023-11-19 09:35:35 +0200

    WR-37 Refactoring

--- app/src/main/java/com/koniukhov/waterreminder/fragments/StarterFragment.kt
+++ app/src/main/java/com/koniukhov/waterreminder/fragments/StarterFragment.kt
@@ -31,7 +31,6 @@ class StarterFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
