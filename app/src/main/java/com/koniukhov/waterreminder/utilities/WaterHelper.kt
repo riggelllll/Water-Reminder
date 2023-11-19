a57b8992c31b5b494362406145ac0e9f0e38ff72
Vladyslav Koniukhov <riggelllll@gmail.com>
2023-11-19 09:50:37 +0200

    WR-37 Refactoring

--- app/src/main/java/com/koniukhov/waterreminder/utilities/WaterHelper.kt
+++ app/src/main/java/com/koniukhov/waterreminder/utilities/WaterHelper.kt
@@ -2,18 +2,15 @@ package com.koniukhov.waterreminder.utilities
 
 import com.koniukhov.waterreminder.data.user.Gender
 
-class WaterHelper {
-    companion object{
-        private const val MALE_COEFFICIENT = 40
-        private const val FEMALE_COEFFICIENT = 30
+object WaterHelper{
+    private const val MALE_COEFFICIENT = 40
+    private const val FEMALE_COEFFICIENT = 30
 
-        fun calculateWaterAmount(gender: Gender, weight: Int): Int{
-            return if (gender == Gender.MALE){
-                weight * MALE_COEFFICIENT
-            }else{
-                weight * FEMALE_COEFFICIENT
-            }
+    fun calculateWaterAmount(gender: Gender, weight: Int): Int{
+        return if (gender == Gender.MALE){
+            weight * MALE_COEFFICIENT
+        }else{
+            weight * FEMALE_COEFFICIENT
         }
     }
-
 }
