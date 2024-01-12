b737b96a74115b44b6a28340bcbd02d1b7692501
Vladyslav Koniukhov <riggelllll@gmail.com>
2024-01-12 10:07:16 +0200

    Added tests

--- app/src/test/java/com/koniukhov/waterreminder/TimeUtilsTest.kt
+++ app/src/test/java/com/koniukhov/waterreminder/TimeUtilsTest.kt
@@ -1,9 +1,12 @@
 package com.koniukhov.waterreminder
 
 import com.koniukhov.waterreminder.utilities.isBetweenLocalTime
+import com.koniukhov.waterreminder.utilities.yearMonthFormat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 import java.time.LocalTime
 
 class TimeUtilsTest {
@@ -33,5 +36,57 @@ class TimeUtilsTest {
         val bedTime = LocalTime.of(18, 0 )
 
         assertFalse(isBetweenLocalTime(now, wakeUp, bedTime))
+    }
+
+    @Test
+    fun yearMonthFormatShouldBe_2024_12(){
+        val yearMonth = yearMonthFormat(LocalDate.of(2024,12,11))
+        assertEquals("2024-12", yearMonth)
+    }
+
+    @Test
+    fun yearMonthFormatShouldBe_2024_01(){
+        val yearMonth = yearMonthFormat(LocalDate.now())
+        assertEquals("2024-01", yearMonth)
+    }
+
+    @Test
+    fun yearMonthFormatSizeShouldBeEqual_7(){
+        var startDate = LocalDate.of(2024,1,1)
+        for (i in 0..365){
+            val yearMonth = yearMonthFormat(startDate)
+            assertEquals(7, yearMonth.length)
+            startDate = startDate.plusDays(1)
+        }
+    }
+
+    @Test
+    fun yearMonthFormatShouldBeJanuary(){
+        var startDate = LocalDate.of(2024,1,1)
+        for (i in 0..25){
+            val yearMonth = yearMonthFormat(startDate)
+            assertEquals("2024-01", yearMonth)
+            startDate = startDate.plusDays(1)
+        }
+    }
+
+    @Test
+    fun yearMonthFormatShouldBeMarch(){
+        var startDate = LocalDate.of(2024,3,1)
+        for (i in 0..25){
+            val yearMonth = yearMonthFormat(startDate)
+            assertEquals("2024-03", yearMonth)
+            startDate = startDate.plusDays(1)
+        }
+    }
+
+    @Test
+    fun yearMonthFormatShouldBeDecember(){
+        var startDate = LocalDate.of(2024,12,1)
+        for (i in 0..25){
+            val yearMonth = yearMonthFormat(startDate)
+            assertEquals("2024-12", yearMonth)
+            startDate = startDate.plusDays(1)
+        }
     }
 }
