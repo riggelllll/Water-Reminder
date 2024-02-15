0ba0ca4c0b407c96cf09682339991f3e968444fa
Vladyslav Koniukhov <riggelllll@gmail.com>
2024-02-15 07:40:44 +0200

    Constants are placed in a separate file

---
+++ app/src/main/java/com/koniukhov/waterreminder/utilities/Constants.kt
@@ -0,0 +1,26 @@
+package com.koniukhov.waterreminder.utilities
+
+object Constants {
+    const val DEFAULT_REMINDER_INTERVAL = 2
+    const val DEFAULT_WEIGHT = 60
+    const val DEFAULT_WAKE_UP_HOUR = 6
+    const val DEFAULT_BED_HOUR = 22
+    const val DEFAULT_MINUTE = 0
+
+    const val HOUR_MIN_VALUE = 0
+    const val HOUR_MAX_VALUE = 23
+    const val MINUTE_MAX_VALUE = 59
+
+    const val WEIGHT_MIN = 20
+    const val WEIGHT_MAX = 200
+    const val WEIGHT_STEP = 1
+
+    const val WATER_LIMIT_FROM = 500
+    const val WATER_LIMIT_TO = 10000
+    const val WATER_LIMIT_STEP = 10
+
+    const val WAKE_UP_EXTRA = "wakeUpTime"
+    const val BED_TIME_EXTRA = "bedTime"
+
+    const val DEFAULT_SEX_ALPHA = 0.5f
+}
